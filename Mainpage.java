7b79a36638ffa6b9156e14678b3fcb37a40c233c
Ulviyya1 <40857015+Ulviyya1@users.noreply.github.com>
2019-06-28 14:54:52 +0200

    Add files via upload

---
+++ Mainpage.java
@@ -0,0 +1,28 @@
+
+public class Mainpage {
+	
+	static String Model;
+	String  Place;
+	String item;
+	String Mood;
+	String clothes;
+	double Salary ;
+    
+	
+	//public void getPlace(){
+	//	System.out.println("");
+		
+	//}
+	  
+	public String getSalary() {
+		String newSalary = String.valueOf(Salary); // 
+		return newSalary;
+		
+		
+	}
+	
+	public void notexp(String X) {
+		System.out.println("I see");
+	}
+
+}
